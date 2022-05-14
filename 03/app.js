1a6f27004433b5875604aa89eaf54a70a99f6e34
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-05-14 19:00:41 +0200

    img load + li added

--- 03/app.js
+++ 03/app.js
@@ -1,14 +1,36 @@
 //(function(){
 
-const images = document.querySelector('.files')
+const imagesEl = document.querySelector('.files')
 const imagesList = document.querySelector('.images-list')
 
-function loadImages() {
+function loadImages(e) {
 
+    const file = e.target.files[0]
 
+    if (file && file.type.includes('image')) {
+
+        const reader = new FileReader()
+
+        reader.onload = function(readerEvent) {
+
+            liItem = imagesList.lastElementChild
+            liItem.classList.remove('images-list__item--prototype')
+
+            imageProperties = liItem.children
+            const sizeInMB = (file.size / (1024*1024)).toFixed(2)
+
+            imageProperties[0].innerText = file.name
+            imageProperties[1].src = readerEvent.target.result
+            imageProperties[2].innerText = (sizeInMB + ' MB')
+
+        }
+
+        reader.readAsDataURL(file)
+
+    }
 
 }
 
-images.lastElementChild.addEventListener('change', loadImages)
+imagesEl.lastElementChild.addEventListener('change', loadImages)
 
 //})()

64c846e3f939ab5a1d2898ff6640159cd18091c8
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-05-14 17:46:46 +0200

    addEventListener added

---
+++ 03/app.js
@@ -0,0 +1,14 @@
+//(function(){
+
+const images = document.querySelector('.files')
+const imagesList = document.querySelector('.images-list')
+
+function loadImages() {
+
+
+
+}
+
+images.lastElementChild.addEventListener('change', loadImages)
+
+//})()
